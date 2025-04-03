a0ec740292d14e5214f1ee66ee92bd7efd7da1d9
Willow et al <185878324+Willow-et-al@users.noreply.github.com>
2025-04-03 06:35:20 +0000

    discourage guessing the pw

--- cogs/ivu.py
+++ cogs/ivu.py
@@ -45,18 +45,26 @@ class Ivu(commands.Cog):
 
 	@app_commands.command(name='password')
 	async def password_command(self, interaction, password: str):
+		grant_role = interaction.guild.get_role(self.bot.config['ids']['grant_role'])
+		if grant_role in interaction.user.roles:
+			await interaction.response.send_message(
+				f'Sorry, but you already have the {grant_role} role. '
+				'To discourage guessing, I will not tell you whether this password is correct.',
+				ephemeral=True,
+			)
+			return
+
 		if password not in self.passwords:
 			await interaction.response.send_message('Wrong password!', ephemeral=True)
 			return
 
-		grant_role = interaction.guild.get_role(self.bot.config['ids']['grant_role'])
 		await interaction.user.add_roles(grant_role)
 
 		if remove_role_id := self.bot.config['ids']['remove_role']:
 			remove_role = interaction.guild.get_role(remove_role_id)
 			await interaction.user.remove_roles(remove_role)
 
-		await interaction.response.send_message('Thanks!', ephemeral=True)
+		await interaction.response.send_message(f'Thanks! You have been granted the {grant_role} role.', ephemeral=True)
 
 	@commands.command(name='set-password')
 	@commands.check(has_ivu_admin_role)
